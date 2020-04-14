d66fbd282b9fbb12bae1df7f5eae14992a827446
Ellen Jian Gao <Ellenisawake@users.noreply.github.com>
2020-04-14 11:46:59 +0100

    Add files via upload

--- run_train.sh
+++ run_train.sh
@@ -1,1 +1,13 @@
-python officehome_norm_aleatoric_nsigmoid_thresh.py --num_epoch 100 --weight_L2norm 0.05 --fc_dim 512 --cls_lr 1.0 --nu_sigmoid 2.0 --tgt_keep 0.6 --device 3 --source Product --target RealWorld;
+python train.py --num_epoch 100 --cls_lr 1.0 --tgt_keep 0.6 --device 0 --data_dir /home/jian/datasets --save_dir /home/jian/results --resnet_model_dir /home/jian/models --source Art --target Clipart;
+python train.py --num_epoch 100 --cls_lr 1.0 --tgt_keep 0.6 --device 0 --data_dir /home/jian/datasets --save_dir /home/jian/results --resnet_model_dir /home/jian/models --source Art --target Product;
+python train.py --num_epoch 100 --cls_lr 1.0 --tgt_keep 0.6 --device 0 --data_dir /home/jian/datasets --save_dir /home/jian/results --resnet_model_dir /home/jian/models --source Art --target RealWorld;
+python train.py --num_epoch 100 --cls_lr 1.0 --tgt_keep 0.6 --device 0 --data_dir /home/jian/datasets --save_dir /home/jian/results --resnet_model_dir /home/jian/models --source Clipart --target Art;
+python train.py --num_epoch 100 --cls_lr 1.0 --tgt_keep 0.6 --device 0 --data_dir /home/jian/datasets --save_dir /home/jian/results --resnet_model_dir /home/jian/models --source Clipart --target Product;
+python train.py --num_epoch 100 --cls_lr 1.0 --tgt_keep 0.6 --device 0 --data_dir /home/jian/datasets --save_dir /home/jian/results --resnet_model_dir /home/jian/models --source Clipart --target RealWorld;
+python train.py --num_epoch 100 --cls_lr 1.0 --tgt_keep 0.6 --device 0 --data_dir /home/jian/datasets --save_dir /home/jian/results --resnet_model_dir /home/jian/models --source Product --target Art;
+python train.py --num_epoch 100 --cls_lr 1.0 --tgt_keep 0.6 --device 0 --data_dir /home/jian/datasets --save_dir /home/jian/results --resnet_model_dir /home/jian/models --source Product --target Clipart;
+python train.py --num_epoch 100 --cls_lr 1.0 --tgt_keep 0.6 --device 0 --data_dir /home/jian/datasets --save_dir /home/jian/results --resnet_model_dir /home/jian/models --source Product --target RealWorld;
+python train.py --num_epoch 100 --cls_lr 1.0 --tgt_keep 0.6 --device 0 --data_dir /home/jian/datasets --save_dir /home/jian/results --resnet_model_dir /home/jian/models --source RealWorld --target Art;
+python train.py --num_epoch 100 --cls_lr 1.0 --tgt_keep 0.6 --device 0 --data_dir /home/jian/datasets --save_dir /home/jian/results --resnet_model_dir /home/jian/models --source RealWorld --target Clipart;
+python train.py --num_epoch 100 --cls_lr 1.0 --tgt_keep 0.6 --device 0 --data_dir /home/jian/datasets --save_dir /home/jian/results --resnet_model_dir /home/jian/models --source RealWorld --target Product;
+
